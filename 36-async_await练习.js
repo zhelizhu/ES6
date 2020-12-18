08960af62996d3c9462f93eb3ec32b867fd85c9a
zhelizhu <1798012151@qq.com>
2020-12-18 13:48:47 +0800

    master

--- 36-async_await练习.js
+++ 36-async_await练习.js
@@ -1,54 +1,58 @@
-// const fs = require('fs')
+const fs = require('fs')
 
-// function read1() {
-//     return new Promise((res, rej) =>{
-//         fs.readFile('./public/1.txt', (err, data)=>{
-//             if (err) {
-//                 rej(err)
-//             }
-//             else{
-//                 res(data)
-//             }
-//         })
-//     })
-// }
+function read1() {
+    return new Promise((res, rej) =>{
+        fs.readFile('./public/1.txt', (err, data)=>{
+            if (err) {
+                rej(err)
+            }
+            else{
+                res(data)
+            }
+        })
+    })
+}
 
-// function read2() {
-//     return new Promise((res, rej) =>{
-//         fs.readFile('./public/2.txt', (err, data)=>{
-//             if (err) {
-//                 rej(err)
-//             }
-//             else{
-//                 res(data)
-//             }
-//         })
-//     })
-// }
+function read2() {
+    return new Promise((res, rej) =>{
+        fs.readFile('./public/2.txt', (err, data)=>{
+            if (err) {
+                rej(err)
+            }
+            else{
+                res(data)
+            }
+        })
+    })
+}
 
-// function read3() {
-//     return new Promise((res, rej) =>{
-//         fs.readFile('./public/3.txt', (err, data)=>{
-//             if (err) {
-//                 rej(err)
-//             }
-//             else{
-//                 res(data)
-//             }
-//         })
-//     })
+function read3() {
+    return new Promise((res, rej) =>{
+        fs.readFile('./public/3.txt', (err, data)=>{
+            if (err) {
+                rej(err)
+            }
+            else{
+                res(data)
+            }
+        })
+    })
 
-// }
+}
 
-// async function main(){
+async function main(){
+    try{
+        let t1=await read1()
 
-//    let t1=await read1()
+        let t2=await read2()
+     
+        let t3=await read3()
+     
+       console.log(t1.toString());
+    }
+    catch{
+        console.log('err');
+    }
+}
 
-//    let t2=await read2()
-
-//    let t3=await read3()
-
-//   console.log(t1.toString());
-// }
-
-// main()
+main()
